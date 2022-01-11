6d44e2c8df22e4db96c8fd6685f37ab79503d814
jleien <jleiendecker@dmacc.com>
2022-01-11 15:30:27 -0600

    add a vehicle and modify vehicleNoise

--- src/VehicleNoises.java
+++ src/VehicleNoises.java
@@ -1,4 +1,5 @@
 import model.Car;
+import model.Plane;
 import model.Vehicle;
 import model.Truck;
 
@@ -16,7 +17,9 @@ public class VehicleNoises {
 
 		Truck toyota = new Truck("Toyota", "Tundra", "Blue", 2003, 217289.7);
 		System.out.println(toyota.makeNoise()); //William Thomas 01/11/2022
-
+		
+		Plane Boeing = new Plane("Boeing 737", "White", 2015);
+		System.out.println(Boeing.makeNoise()); 
 	}
 
 }
